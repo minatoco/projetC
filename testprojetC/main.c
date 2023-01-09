9c53207e6cf1bd35f7c466e13dfd5bbdc42f2bd2
minatoco <102216647+minatoco@users.noreply.github.com>
2023-01-09 13:38:10 +0100

    add fonction

--- testprojetC/main.c
+++ testprojetC/main.c
@@ -4,20 +4,30 @@
 
 typedef struct {
     int id;
-    int role;
     char name[50];
+    int role;
     char password[50];
 } Login;
 
 typedef struct {
     int id;
     char title[50];
-    char description[150];
+    char description[200];
     int status;
+    char service[50];
 } Ticket;
 
+typedef struct {
+    int id_comment; //id_utilisateur
+    int id_ticket;
+    char comment[400];
+}Comment;
+
+Comment commentaire[100];
 Ticket tickets[100];
 int ticket_count = 0;
+int commentaire_count=0;
+
 
 void inputStrings(char *string,int size){
     fgets(string,size,stdin);
@@ -30,14 +40,73 @@ void inputStrings(char *string,int size){
 
 void print_ticket(Ticket ticket) {
 
+
     printf("ID: %d\n", ticket.id);
+    printf("Service : %s\n",ticket.service);
     printf("Titre: %s\n", ticket.title);
     printf("Description: %s\n", ticket.description);
+
     if(ticket.status == 1){
         printf("Status: Ferme\n");
     }else{
         printf("Status: Ouvert\n");
     }
+
+}
+
+void addcomment(int id_ticket){
+    Comment comments;
+    comments.id_ticket=id_ticket;
+    comments.id_comment=commentaire_count+1;
+
+    printf("veuillez saisir votre commentaire:\n");
+    inputStrings(comments.comment,400);
+
+
+    commentaire[commentaire_count++] = comments;
+}
+
+void view_ticket(int ticket_id) {
+    int choicerespons;
+
+    Comment comments;
+    //ajoute des ancien commentaire
+    for (int i = 0; i < commentaire_count; ++i) {
+        if (ticket_id == commentaire[i].id_ticket) {
+            printf("\n");
+            printf("ID du ticket : %d\n", commentaire[i].id_ticket);
+            printf("ID du commentaire : %d\n", commentaire[i].id_comment);
+            printf("Commentaire :%s\n", commentaire[i].comment);
+        }
+    }// fin ajout
+    while(1) {
+        printf("\n");
+        printf("1. Ajouter un commentaire\n");  // choix
+        printf("0. Quitter\n\n");
+        scanf("%d", &choicerespons);
+        fflush(stdin);
+        if (choicerespons == 0) {
+            return;
+        }
+        if(choicerespons == 1){
+            addcomment(ticket_id);
+            print_ticket(tickets[ticket_id-1]);
+            for (int i = 0; i < commentaire_count; ++i) {
+                if (ticket_id == commentaire[i].id_ticket) {
+                    printf("\n\n");
+                    printf("ID du ticket : %d\n", commentaire[i].id_ticket);
+                    printf("ID du commentaire : %d\n", commentaire[i].id_comment);
+                    printf("Commentaire :%s\n", commentaire[i].comment);
+                }
+            }
+
+        }else{
+            printf("Erreur lors du choix ");
+        }
+
+
+    }
+
 }
 
 void create_ticket() {
@@ -45,12 +114,43 @@ void create_ticket() {
         printf("Erreur: Impossible de creer un nouveau ticket. Numbre maximun de tickets créable atteint.\n");
         return;
     }
-
+    int service;
     Ticket ticket;
     ticket.id = ticket_count + 1;
 
-    char status;
+    while(1) {
+        printf("Entrer le service qui a un probleme :\n");
+        printf("1.Service de Production\n"
+               "2.Service de Ventes\n"
+               "3.Service de Recherche et Developpement\n"
+               "4.Service de Comptabilite et Finance\n"
+               "5.Service de Ressources Humaines\n");
+        scanf("%d", &service);
+        fflush(stdin);
 
+        if (service == 1) {
+            strcpy(ticket.service, "Service de Production");
+            break;
+        }
+        if (service == 2) {
+            strcpy(ticket.service, "Service de Ventes");
+            break;
+        }
+        if (service == 3) {
+            strcpy(ticket.service, "Service de Recherche et Developpement");
+            break;
+        }
+        if (service == 4) {
+            strcpy(ticket.service, "Service de Comptabilite et Finance");
+            break;
+        }
+        if (service == 5) {
+            strcpy(ticket.service, "Service de Ressources Humaines");
+            break;
+        } else {
+            printf("Erreur sur le lieu u probleme\n\n");
+        }
+    }
     printf("\n");
     printf("Entrer le titre du ticket:");
     inputStrings(ticket.title,50);
@@ -58,7 +158,7 @@ void create_ticket() {
 
     printf("Entrer la description du ticket:");
     inputStrings(ticket.description,200);
-
+    printf("\n");
     ticket.status = 0;
 
     tickets[ticket_count++] = ticket;
@@ -66,6 +166,7 @@ void create_ticket() {
 }
 
 void list_tickets() {
+    int choiceticket;
     if(ticket_count==0){
         printf("Vous n'avez pas de tickets \n\n");
 
@@ -74,7 +175,24 @@ void list_tickets() {
             print_ticket(tickets[i]);
             printf("\n\n");
         }
+        while(1) {
+            printf("Ecrivez le numero du ticket\n");
+            printf("0. Quitter\n");
+            scanf("%d",&choiceticket);
+            if(choiceticket == 0) {
+                return;
+            }
+                for (int i = 0; i < ticket_count; ++i) {
+                    if (choiceticket == tickets[i].id) {
+                        print_ticket(tickets[choiceticket-1]);
+                        view_ticket(tickets[i].id);
+                        list_tickets();
+                    }
+                }
+
+        }
     }
+
 }
 
 void update_ticket_status() {
@@ -85,7 +203,7 @@ void update_ticket_status() {
     for (int i = 0; i < ticket_count; i++) {
         if (tickets[i].id == id) {
             printf("Entrer Nouveau status(0 pour ouvert et 1 pour ferme): ");
-            scanf("%d", tickets[i].status);
+            scanf("%d", &tickets[i].status);
 
             if(tickets[i].status == 1){
                 printf("Ferme\n\n");
@@ -100,24 +218,38 @@ void update_ticket_status() {
 }
 
 int main() {
-
-
+        Ticket ticket;
         Login login;
-        printf("Bonjour Connecter Vous\n");
-        printf("Name : ");
-        inputStrings(login.name,50);
-        printf("\n");
-        printf("Mot de passe :");
-        inputStrings(login.password,50);
-
-
-
-    while (login.name!="admin" && login.password!="admin"){
         int choice;
+        int goMenu = 1;
+        int counterLogin;
+
+       do{
+
+        for(counterLogin = 0;counterLogin<3;++counterLogin){
+
+            printf("Name : ");
+            inputStrings(login.name,50);
+            printf("\n");
+            printf("Mot de passe :");
+            inputStrings(login.password,50);
+            printf("\n\n");
+
+            if(!strcmp(login.name, "admin") && !strcmp(login.password, "admin")){
+                counterLogin = 0;
+                break;
+            }
+        }
+
+
+
+    while (!strcmp(login.name, "admin") && !strcmp(login.password, "admin")){
+
         printf("1. Cree un ticket\n");
         printf("2. Liste des tickets\n");
         printf("3. Changer le status d'un ticket\n");
-        printf("4. Quitter\n");
+        printf("4. Deconnecter\n");
+        printf("5. Quitter\n");
         printf("Entrer Le numero attribue: ");
         scanf("%d", &choice);
         fflush(stdin);
@@ -129,11 +261,16 @@ int main() {
             list_tickets();
         } else if (choice == 3) {
             update_ticket_status();
-        } else if (choice == 4) {
+        } else if (choice == 5) {
+            goMenu=0;
             break;
-        } else {
+        } else if (choice == 4) {
+            return 0;
+        } else
             printf("Error: Invalid choice.\n");
         }
     }
+       while(goMenu == 1);
+
     return 0;
 }
